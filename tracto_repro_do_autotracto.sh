71ed3b33ee984e738101223c832dd5383240b315
Luis Concha <lconcha@gmail.com>
2019-10-07 16:46:05 -0500

    still not ready

--- tracto_repro_do_autotracto.sh
+++ tracto_repro_do_autotracto.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-subject=$1
+
 
 
 dataFolder=/misc/mansfield/lconcha/exp/tracto_repro/nobackup/Data_2.0
+resultsFolder=/misc/mansfield/lconcha/exp/tracto_repro/results
+auto_tracto=/misc/mansfield/lconcha/exp/tracto_repro/code/auto_tracto_fnirt.sh
+
+
+subject=s2
+
+tckIN=${dataFolder}/${subject}/tracking-probabilistic.tck
+thisOutFolder=$resultsFolder/${subject}
+mask=${dataFolder}/${subject}/mask-brain.nii.gz
+fa=${dataFolder}/${subject}/fa.nii.gz
+
+if [ ! -d $thisOutFolder ]
+then
+  echo mkdir $thisOutFolder
+fi
+
+echo $auto_tracto \
+  -tck $tckIN \
+  -mask $mask \
+  -fa $fa \
+  -outbase  $thisOutFolder \
+  -keepROIs
