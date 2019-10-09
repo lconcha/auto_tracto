28307d9751018cbe7d7b8a988650d655d0fd00d7
Luis Concha <lconcha@gmail.com>
2019-10-08 19:18:48 -0500

    ready to do all subjects

--- tracto_repro_do_autotracto.sh
+++ tracto_repro_do_autotracto.sh
@@ -3,26 +3,42 @@
 
 
 
-dataFolder=/misc/mansfield/lconcha/exp/tracto_repro/nobackup/Data_2.0
+dataFolder=/misc/mansfield/lconcha/exp/tracto_repro/nobackup
 resultsFolder=/misc/mansfield/lconcha/exp/tracto_repro/results
-auto_tracto=/misc/mansfield/lconcha/exp/tracto_repro/code/auto_tracto_fnirt.sh
+auto_tracto=/misc/mansfield/lconcha/exp/tracto_repro/auto_tracto/auto_tracto_fnirt.sh
+tmpFolder=/misc/mansfield/lconcha/exp/tracto_repro/tmp
+protocolsFolder=/misc/mansfield/lconcha/exp/tracto_repro/auto_tracto/lanirem
+
+subjects="s1 s2 s3 s4 s5 s6"
 
 
-subject=s2
+for subject in $subjects
+do
+  tckIN=${dataFolder}/${subject}/tracking-probabilistic.tck
+  thisOutFolder=$resultsFolder/${subject}
+  mask=${dataFolder}/${subject}/mask-brain.nii.gz
+  fa=${dataFolder}/${subject}/fa.nii.gz
+  this_tmpDir=${tmpFolder}/${subject}_$$
 
-tckIN=${dataFolder}/${subject}/tracking-probabilistic.tck
-thisOutFolder=$resultsFolder/${subject}
-mask=${dataFolder}/${subject}/mask-brain.nii.gz
-fa=${dataFolder}/${subject}/fa.nii.gz
+  if [ ! -d $thisOutFolder ]
+  then
+     mkdir $thisOutFolder
+  fi
 
-if [ ! -d $thisOutFolder ]
-then
-  echo mkdir $thisOutFolder
-fi
+  $auto_tracto \
+    -tck $tckIN \
+    -mask $mask \
+    -fa $fa \
+    -outbase  $thisOutFolder/ \
+    -keepROIs \
+    -tmpDir $this_tmpDir \
+    -autoPtx $protocolsFolder
 
-echo $auto_tracto \
-  -tck $tckIN \
-  -mask $mask \
-  -fa $fa \
-  -outbase  $thisOutFolder \
-  -keepROIs
+
+  for cmdfile in $thisOutFolder/*.command
+  do
+    sed "s|${this_tmpDir}/||g" $cmdfile | sed 's|.nii |.nii.gz |g' | sed 's|.nii$|.nii.gz|' >  ${cmdfile%.command}_command.txt
+    rm -f $cmdfile
+  done
+
+done
