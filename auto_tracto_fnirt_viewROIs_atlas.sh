65209956bb0d12ae4ff67bdedba6368ad6380832
Luis Concha <lconcha@gmail.com>
2019-11-19 18:38:51 -0600

    added protocol folder

--- auto_tracto_fnirt_viewROIs_atlas.sh
+++ auto_tracto_fnirt_viewROIs_atlas.sh
@@ -2,13 +2,16 @@
 
 
 atlas=${FSLDIR}/data/standard/FMRIB58_FA_1mm.nii.gz
+protocolsFolder=/misc/mansfield/lconcha/exp/tracto_repro/auto_tracto/lanirem/protocols/
+
+
 str=$1
 includes=""
 
 
 
 
-if [ -d ${str}_R ]
+if [ -d ${protocolsFolder}/${str}_R ]
 then
   paired=1
 else
@@ -20,17 +23,17 @@ then
   echo "Viewing a paired structure"
   for side in R L
   do
-    targets=`ls ${str}_${side}/target*`
+    targets=`ls ${protocolsFolder}/${str}_${side}/target*`
     for t in $targets
     do
       includes="$includes -roi.load $t -roi.colour 0,1,0"
     done
   done
    mrview $atlas \
-    -roi.load ${str}_R/seed.ni*    -roi.colour 0,0.2,1 \
-    -roi.load ${str}_R/exclude.ni* -roi.colour 1,0,0 \
-    -roi.load ${str}_L/seed.ni*    -roi.colour 0,0,1 \
-    -roi.load ${str}_L/exclude.ni* -roi.colour 1,0.3,0 \
+    -roi.load ${protocolsFolder}/${str}_R/seed.ni*    -roi.colour 0,0.2,1 \
+    -roi.load ${protocolsFolder}/${str}_R/exclude.ni* -roi.colour 1,0,0 \
+    -roi.load ${protocolsFolder}/${str}_L/seed.ni*    -roi.colour 0,0,1 \
+    -roi.load ${protocolsFolder}/${str}_L/exclude.ni* -roi.colour 1,0.3,0 \
     $includes
 else
    targets=`ls ${str}/target*`
