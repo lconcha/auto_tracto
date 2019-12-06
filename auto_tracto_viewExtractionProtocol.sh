2761401fd620b40e9eb8fd383762f87c516341af
Luis Concha <lconcha@gmail.com>
2019-12-06 09:56:28 -0600

    corrected the display of multiple targets

--- auto_tracto_viewExtractionProtocol.sh
+++ auto_tracto_viewExtractionProtocol.sh
@@ -10,44 +10,48 @@ protocolsFolder=/misc/mansfield/lconcha/exp/tracto_repro/auto_tracto/lanirem
 
 exclusions=""
 n=`ls ${protocolsFolder}/protocols/${structure}/exclude* | wc -l`
+echolor yellow "There are $n exclusions"
 if [ $n -eq 0 ]; then  exclusions=""
 else
   for excl in ${protocolsFolder}/protocols/${structure}/exclude*
   do
-    exclusions="-roi.load $excl -roi.colour 1,0,0 -roi.opacity 0.5"
+    exclusions="$exclusions -roi.load $excl -roi.colour 1,0,0 -roi.opacity 0.5"
   done
 fi
 
 
 seeds=""
 n=`ls ${protocolsFolder}/protocols/${structure}/seed* | wc -l`
+echolor yellow "There are $n seeds"
 if [ $n -eq 0 ]; then  seeds=""
 else
   for seed in ${protocolsFolder}/protocols/${structure}/seed*
   do
-    seeds="-roi.load $seed -roi.colour 0,1,0 -roi.opacity 0.5"
+    seeds="$seeds -roi.load $seed -roi.colour 0,1,0 -roi.opacity 0.5"
     n=$(( $n +1 ))
   done
 fi
 
 targets=""
 n=`ls ${protocolsFolder}/protocols/${structure}/target* | wc -l`
+echolor yellow "There are $n targets"
 if [ $n -eq 0 ]; then  targets=""
 else
   for target in ${protocolsFolder}/protocols/${structure}/target*
   do
-    targets="-roi.load $target -roi.colour 0,1,0 -roi.opacity 0.5"
+    targets="$targets -roi.load $target -roi.colour 0,1,0 -roi.opacity 0.5"
     n=$(( $n +1 ))
   done
 fi
 
 stops=""
 n=`ls ${protocolsFolder}/protocols/${structure}/stop* | wc -l`
+echolor yellow "There are $n stops"
 if [ $n -eq 0 ]; then  stops=""
 else
   for stop in ${protocolsFolder}/protocols/${structure}/stop*
   do
-    stops="-roi.load $stop -roi.colour 0,0,1 -roi.opacity 0.5"
+    stops="$stops -roi.load $stop -roi.colour 0,0,1 -roi.opacity 0.5"
     n=$(( $n +1 ))
   done
 fi
