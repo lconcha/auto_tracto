0600b9f0051da79457146af147fae7e358ad1ed8
Luis Concha <lconcha@gmail.com>
2019-12-14 13:20:59 -0600

    now uses a txt file with the list of tcks, as opposed to a stupid growing string

--- intersect_tck_streamlines.sh
+++ intersect_tck_streamlines.sh
@@ -7,22 +7,22 @@ tckOUT=$1
 shift
 
 
-n=1
-tcksToIntersect=""
+tmpDir=/tmp/intersect_$$
+mkdir $tmpDir
+
+tcksToIntersect=${tmpDir}/tracks_to_intersect.txt
+
 for tck in "$@"
 do
-  echo "  $n : $tck"
-  tcksToIntersect="$tcksToIntersect '$tck',"
-  n=$(( $n + 1 ))
+  echo $tck >> $tcksToIntersect
 done
 echo "  Output will be $tckOUT"
 
-# remove last comma
-tcksToIntersect=${tcksToIntersect::-1}
-
-
 
 $matlab -nodisplay <<EOF
-addpath('/misc/mansfield/lconcha/exp/tracto_repro/auto_tracto')
-intersect_tck_streamlines('$tckOUT',$tcksToIntersect)
+addpath('/misc/mansfield/lconcha/exp/tracto_repro/auto_tracto');
+intersect_tck_streamlines('$tckOUT','$tcksToIntersect')
 EOF
+
+
+rm -fR $tmpDir
